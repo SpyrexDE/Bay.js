3fd58842dcd12807a894b1e1cfb71072b1669da6
Spyrex <malikfk100@gmail.com>
2022-02-03 21:38:57 +0100

    Modular separation

---
+++ src/BayComponent.js
@@ -0,0 +1,219 @@
+export default class BayComponent extends HTMLElement {
+
+    template;
+    style;
+    name;
+    listeners;
+    script;
+    // When true, component data will be applied
+    initialized = false;
+
+    constructor(template, style, name, listeners, script) {
+        super();
+        console.log(template, style, name, listeners, script);
+        if(!template && !style && !name && !listeners && !script)
+            return;
+                
+        this.template = template;
+        this.style = style;
+        this.name = name;
+        this.listeners = listeners;
+        this.script = script;
+
+        this.initialized = true;
+        this.connectedCallback()
+    }
+
+    connectedCallback() {
+        console.log("test: " + this.style)
+        if(!this.initialized)
+            return;
+
+        this._attachShadowRoot();
+
+        // Load in component's script
+        let script = this.script;
+
+        window.eval(script);
+        this._registerFunctions(script);
+        this._registerVariables(script);
+
+        this._addAttributesToData();
+
+        if (typeof this.data !== 'undefined') {
+            this._registerData(script);
+            this._attachListeners(script);
+            this.updateVariables();
+        } else {
+            this._attachListeners(script);
+        }
+    }
+
+    _registerFunctions(script) {
+
+        var lines = script.split("\n");
+        for(var i=0; i < lines.length; i++)
+        {
+            if(!lines[i].includes("function"))
+                continue;
+            
+            this[lines[i].split("function ")[1].split("(")[0]] = eval(lines[i].split("function ")[1].split("(")[0]);
+        }
+    }
+
+    _registerVariables(script) {
+
+        var lines = script.split("\n");
+        for(var i=0; i < lines.length; i++)
+        {
+            const keywords = ["let", "var", "const"];
+            
+            keywords.forEach((keyword) => {
+                if(!lines[i].includes(keyword))
+                    return;
+                
+                // If variable is defined
+                if(lines[i].includes("=")){
+                    const varName = lines[i].split(keyword + " ")[1].split("=")[0].replaceAll(" ", "");
+                    this[varName] = eval(varName);
+                }
+            })
+        }
+    }
+
+    _attachShadowRoot() {
+        this.shadow = this.attachShadow( { mode: 'open' } );
+        let style = this.style;
+
+        console.log(this);
+        window.test = this;
+
+        if(style)
+            this.shadow.appendChild( style );
+        
+        this.shadow.appendChild( document.importNode( this.template.content, true ) );
+    }
+
+    _addAttributesToData() {
+        if(this.attributes.length > 0 && typeof data === 'undefined')
+            this.data = {}
+        for(var i = 0; i < this.attributes.length; i++) {
+            const attribute = this.attributes[i];
+            if (attribute.name.startsWith(":")) {
+                const types = ["Number", "BigInt", "Symbol", "String", "Boolean"]
+                let v = attribute.value;
+
+                for(var k = 0; k < types.length; k++) {
+                    try {
+                        v = eval("new " + types[i] + "(" + v + ")");
+
+                    } catch(Excention) { /*ignore */}
+                }						
+
+                this.data[attribute.name.replace(":", "")] = v;
+            }
+        }
+    }
+
+    _registerData() {
+        let shadow = this.shadow;
+        let o = this;
+
+        // Replace vars with var tags
+        let html = shadow.innerHTML;
+        var htmlLines = html.split("\n");
+
+        for (var i = 0; i < htmlLines.length; i++){
+            let line = htmlLines[i];
+
+            if(line.includes("{{") && line.includes("}}")) {
+                let varName = line.substring(
+                    line.indexOf("{{") + 2, 
+                    line.indexOf("}}")
+                );
+                line = line.replaceAll("{{" + varName + "}}", "<variable " + varName.replaceAll(" ", "") + "></variable>");
+            }
+            htmlLines[i] = line;
+        }
+
+        if(shadow.innerHTML != htmlLines.join("\n")) {
+            shadow.innerHTML = htmlLines.join("\n");
+        }
+        
+        // Register getters and setters for data vars
+        Object.keys(this.data).forEach( ( key ) => {
+
+            Object.defineProperty(this.data, "_" + key, {
+
+                value: this.data[key],
+                writable: true,
+                enumerable: true,
+                configurable: true,
+
+            });
+
+            Object.defineProperty(this.data, key, {
+
+                get () {
+                    if (typeof o["$$_" + key] !== 'undefined') {
+                        const newVal = o["$$_" + key](this["_" + key]);
+                        switch(newVal)
+                        {
+                            case "prevent_default": return;
+                            default: return newVal;
+                        }
+                    }
+                    return this["_" + key];
+                },
+
+                set(value) {
+                    if (typeof o["$_" + key] !== 'undefined') {
+                        const newVal = o["$_" + key](value);
+                        switch(newVal)
+                        {
+                            case "prevent_default": return;
+                            default: this["_" + key] = newVal; o.updateVariables(key); return;
+                        }
+                    }
+
+                    this["_" + key] = value;
+                    
+                    o.updateVariables(key);
+                }
+            });
+        } );
+    }
+
+    updateVariables(key) {
+        if (key == undefined) {
+            Object.keys(this.data).forEach( ( key ) => {
+                this.updateVariables(key);
+            });
+        }
+
+        let shadow = this.shadow;
+        let els = [];
+
+        for(var i = 0; i < shadow.children.length; i++) {
+            let vars = shadow.children[i].getElementsByTagName("variable");
+            
+            for(var k = 0; k < vars.length; k++) {
+                els.push(vars[k]);
+            }
+        }
+        
+        for(var i = 0; i < els.length; i++) {
+            let el = els[i];
+            if(el.attributes[0].name == key) {
+                el.innerText = this.data[key];
+            }
+        }
+    }
+
+    _attachListeners() {
+        let listeners = this.listeners;
+        Object.keys(listeners).forEach( ( event ) => {
+            this.addEventListener( event, this[listeners[event]], false );
+        } );
+    }
+}
--- test/main.js
+++ test/main.js
@@ -1,1 +1,3 @@
+import Bay from "../src/Bay.js";
+
 Bay.loadComponent('components/example-element.html');
